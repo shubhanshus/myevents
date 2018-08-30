8dfd1cb78e7d6adae3b40f3bab41b96e8c3fd859
Shubhanshu Surana <shubhanshu.surana@gmail.com>
2018-08-30 16:46:56 -0400

    updated code to handle new events and locations

--- src/eventservice/rest/rest.go
+++ src/eventservice/rest/rest.go
@@ -3,27 +3,24 @@ package rest
 import (
 	"net/http"
 
-	"github.com/shubhanshus/myevents/src/lib/persistence"
-
 	"github.com/gorilla/mux"
+	"github.com/shubhanshus/myevents/src/lib/msgqueue"
+	"github.com/shubhanshus/myevents/src/lib/persistence"
 )
 
-func ServeAPI(endpoint, tlsendpoint string, databasehandler persistence.DatabaseHandler) (chan error, chan error) {
-	handler := NewEventHandler(databasehandler)
+func ServeAPI(endpoint string, dbHandler persistence.DatabaseHandler, eventEmitter msgqueue.EventEmitter) error {
+	handler := newEventHandler(dbHandler, eventEmitter)
+
 	r := mux.NewRouter()
 	eventsrouter := r.PathPrefix("/events").Subrouter()
-	eventsrouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.FindEventHandler)
-	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.AllEventHandler)
-	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.NewEventHandler)
-	httpErrChan := make(chan error)
-	httptlsErrChan := make(chan error)
+	eventsrouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
+	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
+	eventsrouter.Methods("GET").Path("/{eventID}").HandlerFunc(handler.oneEventHandler)
+	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
 
-	go func() {
-		httptlsErrChan <- http.ListenAndServeTLS(tlsendpoint, "cert.pem", "key.pem", r)
-	}()
-	go func() {
-		httpErrChan <- http.ListenAndServe(endpoint, r)
-	}()
+	locationRouter := r.PathPrefix("/locations").Subrouter()
+	locationRouter.Methods("GET").Path("").HandlerFunc(handler.allLocationsHandler)
+	locationRouter.Methods("POST").Path("").HandlerFunc(handler.newLocationHandler)
 
-	return httpErrChan, httptlsErrChan
+	return http.ListenAndServe(endpoint, r)
 }
